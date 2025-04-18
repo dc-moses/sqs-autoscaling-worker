6bb657a38be36bc4541a87d2170dac25c725f305
Dylan <39346569+dc-moses@users.noreply.github.com>
2025-04-18 15:14:46 -0400

    Update lambda_trigger.py

--- lambda_trigger.py
+++ lambda_trigger.py
@@ -1,4 +1,37 @@
 import json
 import boto3
 import os
-...
+
+sqs = boto3.client('sqs')
+QUEUE_URL = os.environ['QUEUE_URL']
+
+def lambda_handler(event, context):
+    try:
+        body = json.loads(event.get('body', '{}'))
+        wait = int(body.get('wait_seconds', 10))
+
+        message = {
+            "wait_seconds": wait
+        }
+
+        response = sqs.send_message(
+            QueueUrl=QUEUE_URL,
+            MessageBody=json.dumps(message)
+        )
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps({
+                'message': 'Job enqueued',
+                'wait_seconds': wait,
+                'messageId': response['MessageId']
+            })
+        }
+
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({
+                'error': str(e)
+            })
+        }
