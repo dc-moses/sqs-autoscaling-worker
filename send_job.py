40968d221cdb6ddb56a757446c6772657c20c1e5
Dylan <39346569+dc-moses@users.noreply.github.com>
2025-04-18 15:13:57 -0400

    Update send_job.py

--- send_job.py
+++ send_job.py
@@ -1,4 +1,21 @@
 import boto3
 import json
 import argparse
-...
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--queue-url', required=True, help='SQS Queue URL')
+parser.add_argument('--wait', type=int, default=10, help='Wait time in seconds')
+args = parser.parse_args()
+
+sqs = boto3.client('sqs')
+
+message = {
+    "wait_seconds": args.wait
+}
+
+response = sqs.send_message(
+    QueueUrl=args.queue_url,
+    MessageBody=json.dumps(message)
+)
+
+print(f"Sent message with wait={args.wait} seconds. MessageId: {response['MessageId']}")
